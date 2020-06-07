d0a8e4aeada766330ff7dd4d8fe11bbddea96d6e
Patrycja <patrycja.slaby@gmail.com>
2020-06-07 21:32:30 +0200

    excercise1

---
+++ src/homework_day_one/ex1/Main.java
@@ -0,0 +1,18 @@
+package homework_day_one.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        short sub = 7 - 10;
+        short div = 4 / 3;
+        double divFloatPoint = 4.0 / 3;
+        short add = 'a' + 2;
+        String addQuote = "a" + 3;
+        short modulo = 9 % 2;
+        System.out.println(sub);
+        System.out.println(div);
+        System.out.println(divFloatPoint);
+        System.out.println(add);
+        System.out.println(addQuote);
+        System.out.println(modulo);
+    }
+}
