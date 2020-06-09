fd633a0b77b0b870bbf7e1c9ea0ea4129e0e5b79
Patrycja <patrycja.slaby@gmail.com>
2020-06-09 14:04:06 +0200

    excercise7

--- src/homework_day_one/ex7/Main.java
+++ src/homework_day_one/ex7/Main.java
@@ -1,4 +1,22 @@
 package homework_day_one.ex7;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wpisz liczbę w przedziale od 0 do 20");
+        Scanner myScanner = new Scanner(System.in);
+        int number = myScanner.nextInt();
+        if (number <= 8) {
+            System.out.println("Otrzymana ocena to 1");
+        } else if (number == 9 || number == 10) {
+            System.out.println("Otrzymana ocena to 2");
+        } else if (number <= 13) {
+            System.out.println("Otrzymana ocena to 3");
+        } else if (number <= 16) {
+            System.out.println("Otrzymana ocena to 4");
+        } else if (number > 17 ) {
+            System.out.println("Otrzymana ocena to 5");
+        }
+    }
 }
